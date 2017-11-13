ac9016b7bc44a27db149db8d121c0360ee904e46
Ricky Chen <rchen10@stuy.edu>
2017-11-13 01:27:59 +0000

    Wrote parse func

--- parse.c
+++ parse.c
@@ -2,11 +2,22 @@
 #include <stdlib.h>
 #include <string.h>
 
-char ** parse_args( char * line );
+char ** parse_args( char *line ){
+  printf("1\n");
+  char **args = calloc(5, 1);
+  char **pos = args;
+  while (line){
+    printf("2\n");
+    *pos = strsep(&line, " ");
+    pos++;
+    printf("3\n");
+  }
+  return args;
+}
 
 int main(){
-    char line[100] = "wow-this-is-cool";
-    char *s1 = line;
-    printf("[%s]\n", strsep( &s1, "-" ));
-    printf("[%s]\n", s1);
+  printf("1\n");
+  char non_lit[100] = "ls -a -l";
+  char **args = parse_args(non_lit);
+  printf("%s\t%s\t%s\t\n", args[0], args[1], args[2]);
 }
